ce254446284e47059dcfb19a09b4bc78c7be88f9
Usef Farahmand <yoseffarahmand94@gmail.com>
2023-12-30 13:28:10 +0330

    feat create PanelEvent

---
+++ Runtime/PanelEvent.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace UScreens
+{
+    [RequireComponent(typeof(UPanel))]
+    internal class PanelEvent : MonoBehaviour
+    {
+        [SerializeField] private UnityEvent OnShow;
+        [SerializeField] private UnityEvent<float> OnHide;
+
+        public void DoShow() => OnShow?.Invoke();
+        public void DoHide(float duration) => OnHide?.Invoke(duration);
+    }
+}
--- Runtime/UPanel.cs
+++ Runtime/UPanel.cs
@@ -15,10 +15,11 @@ namespace UScreens
 
         [NonSerialized] protected float currentHideDuration = .1f;
 
+        private PanelEvent _Event;
 
         public virtual IUPanelAnim GetPanelAnim() => new AnimatorPanelAnim();
 
-        protected virtual void OnValidate() 
+        protected virtual void OnValidate()
         {
 #if UNITY_EDITOR
             (panelAnim ??= GetPanelAnim())?.Validate(this);
@@ -27,16 +28,18 @@ namespace UScreens
 
         public virtual void Initialize()
         {
-            if(hideBtn)
+            if (hideBtn)
                 hideBtn.onClick.AddListener(HideClicked);
             (panelAnim = GetPanelAnim())?.Initialize(this);
+            _Event = GetComponent<PanelEvent>();
         }
 
         public virtual void Show()
         {
-            panelAnim?.Show();
-            transform.SetAsLastSibling();
             gameObject.SetActive(true);
+            transform.SetAsLastSibling();
+            panelAnim?.Show();
+            _Event?.DoShow();
         }
 
         public virtual void Hide()
@@ -51,6 +54,8 @@ namespace UScreens
             }
             else
                 HideForce();
+
+            _Event?.DoHide(currentHideDuration);
         }
 
         private IEnumerator HideAnimation()
